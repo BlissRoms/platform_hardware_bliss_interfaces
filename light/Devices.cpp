aad3abdb5b5f16ea04b46dca670131f876d2fcb0
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-02-24 09:31:49 +0100

    aidl: light: Treat keyboard devices as keyboards
    Gotta love sliding keyboards acting as a navigation button light
    
    Change-Id: Icff0e78b9dc137065955a6228b9e00d5e6e52bfb
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- light/Devices.cpp
+++ light/Devices.cpp
@@ -58,8 +58,6 @@ static const std::string kButtonLedDevices[] = {
         "button-backlight",
         "button-backlight1",
         "button-backlight2",
-        "keyboard-backlight",
-        "keyboard-backlight-ap",
 };
 
 static std::vector<LedDevice> getButtonLedDevices() {
@@ -70,6 +68,24 @@ static std::vector<LedDevice> getButtonLedDevices() {
         if (button.exists()) {
             LOG(INFO) << "Found button LED device: " << button.getName();
             devices.emplace_back(button);
+        }
+    }
+
+    return devices;
+}
+
+static const std::string kKeyboardLedDevices[] = {
+        "keyboard-backlight",
+};
+
+static std::vector<LedDevice> getKeyboardLedDevices() {
+    std::vector<LedDevice> devices;
+
+    for (const auto& device : kKeyboardLedDevices) {
+        LedDevice keyboard(device);
+        if (keyboard.exists()) {
+            LOG(INFO) << "Found keyboard LED device: " << keyboard.getName();
+            devices.emplace_back(keyboard);
         }
     }
 
@@ -122,6 +138,7 @@ Devices::Devices()
     : mBacklightDevices(getBacklightDevices()),
       mBacklightLedDevices(getBacklightLedDevices()),
       mButtonLedDevices(getButtonLedDevices()),
+      mKeyboardLedDevices(getKeyboardLedDevices()),
       mNotificationRgbLedDevices(getNotificationRgbLedDevices()),
       mNotificationLedDevices(getNotificationLedDevices()) {
     if (!hasBacklightDevices()) {
@@ -130,6 +147,10 @@ Devices::Devices()
 
     if (!hasButtonDevices()) {
         LOG(INFO) << "No button devices found";
+    }
+
+    if (!hasKeyboardDevices()) {
+        LOG(INFO) << "No keyboard devices found";
     }
 
     if (!hasNotificationDevices()) {
@@ -143,6 +164,10 @@ bool Devices::hasBacklightDevices() const {
 
 bool Devices::hasButtonDevices() const {
     return !mButtonLedDevices.empty();
+}
+
+bool Devices::hasKeyboardDevices() const {
+    return !mKeyboardLedDevices.empty();
 }
 
 bool Devices::hasNotificationDevices() const {
@@ -160,6 +185,12 @@ void Devices::setBacklightColor(rgb color) {
 
 void Devices::setButtonsColor(rgb color) {
     for (auto& device : mButtonLedDevices) {
+        device.setBrightness(color.toBrightness());
+    }
+}
+
+void Devices::setKeyboardColor(rgb color) {
+    for (auto& device : mKeyboardLedDevices) {
         device.setBrightness(color.toBrightness());
     }
 }
@@ -199,6 +230,13 @@ void Devices::dump(int fd) const {
         dprintf(fd, "\n");
     }
     dprintf(fd, "\n");
+
+    dprintf(fd, "Keyboard LED devices:\n");
+    for (const auto& device : mKeyboardLedDevices) {
+        dprintf(fd, "- ");
+        device.dump(fd);
+        dprintf(fd, "\n");
+    }
 
     dprintf(fd, "Notification RGB LED devices:\n");
     for (const auto& device : mNotificationRgbLedDevices) {
